7a72f2ecc35ec6af41e1717b280e9acf9a15d904
rameezhashone21 <rameez.hashone@gmail.com>
2022-07-15 21:20:46 +0500

    cart and checkout banner portioned

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,10 +2,11 @@
 <html>
 <head>
     <title>GotDemBeatz - @yield('title')</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="{{ asset('frontend_assets/css/plugins.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend_assets/css/theme.css') }}">
@@ -21,8 +22,12 @@
     </main>    
   <script src="{{ asset('frontend_assets/js/footer.js') }}"></script>     
 </body>
-<!-- <script src="{{ asset('frontend_assets/js/plugins.js') }} " defer></script>
-<script src="{{ asset('frontend_assets/js/theme.js') }} " defer></script>
+
+<!-- <script src="{{ asset('frontend_assets/js/plugins.js') }} " defer></script> -->
+<!-- <script src="{{ asset('frontend_assets/js/theme.js') }} " defer></script>   -->
+
+
+<!--
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/wavesurfer.js"></script> -->
 </html>

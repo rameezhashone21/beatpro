b31fe0045d8cad4f6899351b0159294f3b35053c
rameezhashone21 <rameez.hashone@gmail.com>
2022-07-15 17:09:12 +0500

    cart and checkout banner portioned

--- resources/views/pages/services/index.blade.php
+++ resources/views/pages/services/index.blade.php
@@ -15,28 +15,6 @@
         <section class="services services_section_inner_one membership">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-6 z-index-1 mb-4 mb-md-0">
-                        <div class="position-relative text-center">
-                            <img src="../storage/services/{{ $service->image }}" alt="" class="img-fluid ">
-                            <img src="{{ asset('frontend_assets/img/img_inner_1_fixed.png') }}" alt="" class="img-fluid position-absolute img_fixed_one">
-                        </div>
-                    </div>
-                    <div class="col-md-6 align-self-center">
-                        <h3 class="fs-1 position-relative new_title">{{ $service->name }}</h3>
-                        <div class="text_dark my-4">
-                            {!! $service->desc !!}
-                        </div>
-                        <div class="mt-4">
-                            <a href="#" class="btn-1 text-white">Add To Cart</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        @else
-        <section class="services services_section_inner_one">
-            <div class="container">
-                <div class="row">
                     <div class="col-md-6 align-self-center mb-4 mb-md-0">
                         <h3 class="fs-1 position-relative new_title text-white">{{ $service->name }}</h3>
                         <div class="my-4">
@@ -50,6 +28,28 @@
                         <div class="position-relative text-center">
                             <img src="../storage/services/{{ $service->image }}" alt="" class="img-fluid ">
                             <img src="{{ asset('frontend_assets/img/img_inner_1_fixed.png') }}" alt="" class="img-fluid position-absolute img_fixed_one two">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        @else
+        <section class="services services_section_inner_one">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 z-index-1 mb-4 mb-md-0">
+                        <div class="position-relative text-center">
+                            <img src="../storage/services/{{ $service->image }}" alt="" class="img-fluid ">
+                            <img src="{{ asset('frontend_assets/img/img_inner_1_fixed.png') }}" alt="" class="img-fluid position-absolute img_fixed_one">
+                        </div>
+                    </div>
+                    <div class="col-md-6 align-self-center">
+                        <h3 class="fs-1 position-relative new_title">{{ $service->name }}</h3>
+                        <div class="text_dark my-4">
+                            {!! $service->desc !!}
+                        </div>
+                        <div class="mt-4">
+                            <a href="#" class="btn-1 text-white">Add To Cart</a>
                         </div>
                     </div>
                 </div>
